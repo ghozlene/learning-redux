910facbeb0073bc44d837331d77d1a712a79fb3b
Achref <medachrefg@gmail.com>
2022-08-19 14:27:18 +0100

    working with @redux-toolkit&adding state slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,27 @@
 import { createStore } from 'redux';
+import { createSlice } from '@reduxjs/toolkit';
+
 const initialState = { counter: 0, showCounter: true };
+
+createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment(state) {
+			state.counter++;
+		},
+		increase(state, action) {
+			state.counter = state.counter + action.amount;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
+
 const counterReducer = (state = initialState, action) => {
 	if (action.type === 'increment') {
 		return { counter: state.counter + 1, showCounter: state.showCounter };
