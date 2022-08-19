ff9cbb0828dd595d053d38651ccd82ef5ad307e9
Achref <medachrefg@gmail.com>
2022-08-19 12:56:03 +0100

    using multiple state props

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -4,7 +4,10 @@ import classes from './Counter.module.css';
 const Counter = () => {
 	const dispatch = useDispatch();
 	const counter = useSelector((state) => state.counter);
-	const toggleCounterHandler = () => {};
+	const show = useSelector((state) => state.showCounter);
+	const toggleCounterHandler = () => {
+		dispatch({ type: 'toggle' });
+	};
 
 	const incrementHandler = () => {
 		dispatch({ type: 'increment' });
@@ -19,7 +22,7 @@ const Counter = () => {
 	return (
 		<main className={classes.counter}>
 			<h1>Redux Counter</h1>
-			<div className={classes.value}>{counter} </div>
+			{show && <div className={classes.value}>{counter} </div>}
 			<div>
 				<button onClick={incrementHandler}>increment</button>
 				<button onClick={increaseHandler}>increase by 10</button>
